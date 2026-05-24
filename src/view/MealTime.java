5db56ab1c550643ef9a9774e751e40f075a58807
hilsonlimbu <hilsonlimbu@gmail.com>
2026-05-24 10:45:00 +0545

    style: Style MealTime sidebar buttons with hover colors

--- src/view/MealTime.java
+++ src/view/MealTime.java
@@ -1,7 +1,9 @@
 package view;
 import javax.swing.*;
 import java.awt.*;
+import java.util.logging.Logger;
 public class MealTime extends javax.swing.JFrame {
+    private static final Logger logger = Logger.getLogger(MealTime.class.getName());
     public MealTime() {
         initComponents();
     }
