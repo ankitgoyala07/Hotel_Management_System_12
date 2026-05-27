401c5cc29557867b0b80496fcf71dd736fe1ca3d
hilsonlimbu <hilsonlimbu@gmail.com>
2026-05-27 13:10:00 +0545

    feat: Add user status and action buttons to MealTime header

--- src/view/MealTime.java
+++ src/view/MealTime.java
@@ -6,12 +6,20 @@ public class MealTime extends javax.swing.JFrame {
     private static final Logger logger = Logger.getLogger(MealTime.class.getName());
     private JPanel jPanelSidebar;
     private JPanel jPanelContent;
+    private JButton jButtonDashboard;
+    private JButton jButtonRooms;
+    private JButton jButtonBookings;
+    private JButton jButtonMealTime;
     public MealTime() {
         initComponents();
     }
     private void initComponents() {
         jPanelSidebar = new JPanel();
         jPanelContent = new JPanel();
+        jButtonDashboard = new JButton();
+        jButtonRooms = new JButton();
+        jButtonBookings = new JButton();
+        jButtonMealTime = new JButton();
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setSize(900, 600);
     }
