dd1463ffd9dd02a3127462f5a0d8e874533289bb
hilsonlimbu <hilsonlimbu@gmail.com>
2026-06-06 11:10:00 +0545

    feat: Add header with user manager profile pill

--- src/view/reports.java
+++ src/view/reports.java
@@ -2,5 +2,11 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 public class reports extends javax.swing.JFrame {
-    public reports() {}
+    public reports() {
+        initComponents();
+    }
+    private void initComponents() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setSize(930, 650);
+    }
 }
