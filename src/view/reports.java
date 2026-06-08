4517bb8dedd23274ab87ae7c0a219cc23678766f
hilsonlimbu <hilsonlimbu@gmail.com>
2026-06-08 14:25:00 +0545

    feat: Create floor status chart panel and legend labels

--- src/view/reports.java
+++ src/view/reports.java
@@ -1,7 +1,9 @@
 package view;
 import javax.swing.*;
 import java.awt.*;
+import java.util.logging.Logger;
 public class reports extends javax.swing.JFrame {
+    private static final Logger logger = Logger.getLogger(reports.class.getName());
     public reports() {
         initComponents();
     }
