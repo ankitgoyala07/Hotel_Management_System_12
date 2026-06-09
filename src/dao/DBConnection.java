202354ce58fc0c6abfb34008e24b7d51d30a71d2
rikeshchand42-art <rikeshchand42@gmail.com>
2026-06-09 11:11:25 +0545

    booking Ui updated

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     
-    private static final String URL = "jdbc:mysql://localhost:3306/hotel_management";
+    private static final String URL = "jdbc:mysql://localhost:3306/shop";
     private static final String USER = "root";
     private static final String PASSWORD = "1234"; 
     
