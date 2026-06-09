90aad1257a2b0efa1d6c6c8953eedd1e29cd264e
hilsonlimbu <hilsonlimbu@gmail.com>
2026-06-09 16:50:00 +0545

    feat: Create revenue summary card with monthly stats

--- src/view/reports.java
+++ src/view/reports.java
@@ -4,10 +4,14 @@ import java.awt.*;
 import java.util.logging.Logger;
 public class reports extends javax.swing.JFrame {
     private static final Logger logger = Logger.getLogger(reports.class.getName());
+    private JPanel jPanel1;
+    private JPanel jPanel2;
     public reports() {
         initComponents();
     }
     private void initComponents() {
+        jPanel1 = new JPanel();
+        jPanel2 = new JPanel();
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setSize(930, 650);
     }
