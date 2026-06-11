77b7b546bbb12a1eb64b10fb3b7c4ce729c7006a
hilsonlimbu <hilsonlimbu@gmail.com>
2026-06-11 11:30:00 +0545

    feat: Create guest testimonials card with user reviews

--- src/view/reports.java
+++ src/view/reports.java
@@ -6,12 +6,18 @@ public class reports extends javax.swing.JFrame {
     private static final Logger logger = Logger.getLogger(reports.class.getName());
     private JPanel jPanel1;
     private JPanel jPanel2;
+    private JButton jButton1;
+    private JButton jButton2;
+    private JButton jButton3;
     public reports() {
         initComponents();
     }
     private void initComponents() {
         jPanel1 = new JPanel();
         jPanel2 = new JPanel();
+        jButton1 = new JButton();
+        jButton2 = new JButton();
+        jButton3 = new JButton();
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setSize(930, 650);
     }
