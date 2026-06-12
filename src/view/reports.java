7def26b772b58298a9b9307d8f8a13435ee9a1ba
hilsonlimbu <hilsonlimbu@gmail.com>
2026-06-12 15:20:00 +0545

    feat: Create monthly occupancy chart card with bar graphics

--- src/view/reports.java
+++ src/view/reports.java
@@ -6,6 +6,7 @@ public class reports extends javax.swing.JFrame {
     private static final Logger logger = Logger.getLogger(reports.class.getName());
     private JPanel jPanel1;
     private JPanel jPanel2;
+    private JPanel jPanel3;
     private JButton jButton1;
     private JButton jButton2;
     private JButton jButton3;
@@ -15,6 +16,7 @@ public class reports extends javax.swing.JFrame {
     private void initComponents() {
         jPanel1 = new JPanel();
         jPanel2 = new JPanel();
+        jPanel3 = new JPanel();
         jButton1 = new JButton();
         jButton2 = new JButton();
         jButton3 = new JButton();
