d1f48109c9ba88d81b34a1e18711ae028dd0eb78
rikeshchand42 <rikeshchand42@gmail.com>
2026-06-13 10:30:00 +0545

    ﻿refactor: Document roommanagementModel constructors

--- src/model/roommanagementModel.java
+++ src/model/roommanagementModel.java
@@ -1,4 +1,8 @@
-package model;
+﻿package model;
+
+/**
+ * Model representation of a Room entity with fields for number, type, price, and status
+ */
 
 public class roommanagementModel {
     private final String roomNumber;
